3f177c128e400620d886497b1da4595731c66d04
jpabloallin <jpac.647@gmail.com>
2022-05-02 19:32:49 -0500

    Initial commit BackEnd To-do App project

---
+++ src/main/java/com/todoapp/backend/ToDoAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.todoapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todoapp/backend/ToDoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
